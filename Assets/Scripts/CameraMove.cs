f883d88295202b1a83afef38e07db3193d877d76
johannesfk <9113020+johannesfk@users.noreply.github.com>
2024-12-09 01:36:48 +0100

    Some things working

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CameraMove : MonoBehaviour
+{
+
+    private PlayerInput playerInput;
+    private InputAction moveAction;
+    private InputAction lookAction;
+    private Vector2 moveInput;
+    private Vector2 lookInput;
+
+    private const float moveSpeed = 7.5f;
+    private const float cameraSpeed = 0.5f;
+
+    public Quaternion TargetRotation { private set; get; }
+    private Vector3 targetEuler;
+    private Vector3 moveVector = Vector3.zero;
+    private float moveY = 0.0f;
+
+    private new Rigidbody rigidbody;
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, 0.5f);
+        Gizmos.DrawRay(transform.position, transform.forward * 2);
+    }
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+        if (playerInput != null)
+        {
+            moveAction = playerInput.actions["Move"];
+            lookAction = playerInput.actions["Look"];
+        }
+
+        Cursor.lockState = CursorLockMode.Locked;
+        TargetRotation = transform.rotation;
+    }
+
+    private void OnEnable()
+    {
+        moveAction?.Enable();
+        lookAction?.Enable();
+
+        if (moveAction != null)
+        {
+            moveAction.performed += HandleMove;
+            moveAction.canceled += HandleMove;
+        }
+
+        if (lookAction != null)
+        {
+            lookAction.performed += HandleLook;
+            lookAction.canceled += HandleLook;
+        }
+    }
+
+    private void OnDisable()
+    {
+        moveAction?.Disable();
+        lookAction?.Disable();
+
+        if (moveAction != null)
+        {
+            moveAction.performed -= HandleMove;
+            moveAction.canceled -= HandleMove;
+        }
+
+        if (lookAction != null)
+        {
+            lookAction.performed -= HandleLook;
+            lookAction.canceled -= HandleLook;
+        }
+    }
+
+    private void HandleMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        moveVector = new Vector3(moveInput.x, 0.0f, moveInput.y) * moveSpeed;
+    }
+
+    private void HandleLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+        var rotation = new Vector2(-lookInput.y, lookInput.x);
+        targetEuler = TargetRotation.eulerAngles + (Vector3)rotation * cameraSpeed;
+        TargetRotation = Quaternion.Euler(targetEuler);
+    }
+
+    private void Update()
+    {
+        // Apply smooth camera rotation
+        if (targetEuler.x > 180.0f)
+        {
+            targetEuler.x -= 360.0f;
+        }
+        targetEuler.x = Mathf.Clamp(targetEuler.x, -75.0f, 75.0f);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, TargetRotation,
+            Time.deltaTime * 15.0f);
+    }
+
+    /* private void Update()
+    {
+        // Rotate the camera.
+        var rotation = new Vector2(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"));
+        var targetEuler = TargetRotation.eulerAngles + (Vector3)rotation * cameraSpeed;
+        if (targetEuler.x > 180.0f)
+        {
+            targetEuler.x -= 360.0f;
+        }
+        targetEuler.x = Mathf.Clamp(targetEuler.x, -75.0f, 75.0f);
+        TargetRotation = Quaternion.Euler(targetEuler);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, TargetRotation,
+            Time.deltaTime * 15.0f);
+
+        // Move the camera.
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        moveVector = new Vector3(x, 0.0f, z) * moveSpeed;
+
+        moveY = Input.GetAxis("Elevation");
+    }
+ */
+    private void FixedUpdate()
+    {
+        Vector3 newVelocity = transform.TransformDirection(moveVector);
+        newVelocity.y += moveY * moveSpeed;
+        rigidbody.linearVelocity = newVelocity;
+    }
+
+    public void ResetTargetRotation()
+    {
+        TargetRotation = Quaternion.LookRotation(transform.forward, Vector3.up);
+    }
+}
---
+++ Assets/Scripts/PortalPlacement.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CameraMove))]
+public class PortalPlacement : MonoBehaviour
+{
+
+    private PlayerInput playerInput;
+    private InputAction fireBluePortalAction;
+    private InputAction fireOrangePortalAction;
+
+    [SerializeField]
+    private PortalPair portals;
+
+    [SerializeField]
+    private LayerMask layerMask;
+
+    [SerializeField]
+    private Crosshair crosshair;
+
+    private CameraMove cameraMove;
+
+    private void Awake()
+    {
+        cameraMove = GetComponent<CameraMove>();
+        playerInput = GetComponent<PlayerInput>();        // Get references to the actions
+        fireBluePortalAction = playerInput.actions["FireBluePortal"];
+        fireOrangePortalAction = playerInput.actions["FireOrangePortal"];
+        layerMask = 1 << LayerMask.NameToLayer("LevelGeom");
+    }
+
+    private void OnEnable()
+    {
+        fireBluePortalAction.performed += _ => FirePortal(0, transform.position, transform.forward, 250.0f);
+        fireOrangePortalAction.performed += _ => FirePortal(1, transform.position, transform.forward, 250.0f);
+    }
+
+    private void OnDisable()
+    {
+        fireBluePortalAction.performed -= _ => FirePortal(0, transform.position, transform.forward, 250.0f);
+        fireOrangePortalAction.performed -= _ => FirePortal(1, transform.position, transform.forward, 250.0f);
+    }
+
+    private void FirePortal(int portalID, Vector3 pos, Vector3 dir, float distance)
+    {
+        RaycastHit hit;
+        bool didHit = Physics.Raycast(pos, dir, out hit, distance, layerMask);
+
+        // Add debug ray visualization
+        Debug.DrawRay(pos, dir * distance, didHit ? Color.green : Color.red, 2.0f);
+
+        // Add debug logging
+        Debug.Log($"Raycast: Hit={didHit}, Distance={distance}, LayerMask={layerMask.value}");
+        if (didHit)
+        {
+            Debug.Log($"Hit object: {hit.collider.gameObject.name}");
+        }
+
+        if (hit.collider != null)
+        {
+            Debug.Log("Hit " + hit.collider.name);
+            // If we shoot a portal, recursively fire through the portal.
+            if (hit.collider.tag == "Portal")
+            {
+                Debug.Log("Hit portal");
+                var inPortal = hit.collider.GetComponent<Portal>();
+
+                if (inPortal == null)
+                {
+                    return;
+                }
+
+                var outPortal = inPortal.OtherPortal;
+
+                // Update position of raycast origin with small offset.
+                Vector3 relativePos = inPortal.transform.InverseTransformPoint(hit.point + dir);
+                relativePos = Quaternion.Euler(0.0f, 180.0f, 0.0f) * relativePos;
+                pos = outPortal.transform.TransformPoint(relativePos);
+
+                // Update direction of raycast.
+                Vector3 relativeDir = inPortal.transform.InverseTransformDirection(dir);
+                relativeDir = Quaternion.Euler(0.0f, 180.0f, 0.0f) * relativeDir;
+                dir = outPortal.transform.TransformDirection(relativeDir);
+
+                distance -= Vector3.Distance(pos, hit.point);
+
+                FirePortal(portalID, pos, dir, distance);
+
+                return;
+            }
+
+            // Orient the portal according to camera look direction and surface direction.
+            var cameraRotation = cameraMove.TargetRotation;
+            var portalRight = cameraRotation * Vector3.right;
+
+            if (Mathf.Abs(portalRight.x) >= Mathf.Abs(portalRight.z))
+            {
+                portalRight = (portalRight.x >= 0) ? Vector3.right : -Vector3.right;
+            }
+            else
+            {
+                portalRight = (portalRight.z >= 0) ? Vector3.forward : -Vector3.forward;
+            }
+
+            var portalForward = -hit.normal;
+            var portalUp = -Vector3.Cross(portalRight, portalForward);
+
+            var portalRotation = Quaternion.LookRotation(portalForward, portalUp);
+
+            // Attempt to place the portal.
+            bool wasPlaced = portals.Portals[portalID].PlacePortal(hit.collider, hit.point, portalRotation);
+
+            if (wasPlaced)
+            {
+                Debug.Log("Placed portal");
+                crosshair.SetPortalPlaced(portalID, true);
+            }
+        }
+    }
+}
